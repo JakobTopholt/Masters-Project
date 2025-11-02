ea4c554f9e4b14d909b43c66a4236ddb10aeb3e5
Yusrat Adebisi Jimoh <jimohyusrat@yahoo.com>
2025-11-02 17:04:52 +0100

    Add Result page and route in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import TypingTest from "./pages/TypingTest";
+import ResultPage from "./pages/result";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/typing-test" element={<TypingTest />} />
+        <Route path="/result" element={<ResultPage />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/result.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ResultPage() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Typing Experiment Results</h1>
+      <p>This is where we'll display user results.</p>
+    </div>
+  );
+}
